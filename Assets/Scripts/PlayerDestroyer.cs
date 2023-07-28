c93698574aa55c3e3ab675fcfc8132f84e04c8ce
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2023-07-28 16:38:40 -0400

    player destroyer in progres

--- Assets/Scripts/PlayerDestroyer.cs
+++ Assets/Scripts/PlayerDestroyer.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayerDestroyer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject[] players;
+    private List<Vector3> positions;
+    private bool watchingPlayers;
     void Start()
     {
-        
+           Timer.instance.onTimerComplete()
     }
-
-    // Update is called once per frame
-    void Update()
+    private void startListening()
     {
-        
+        watchingPlayers = true;
+        positions = new List<Vector3>(players.Length);
+        foreach (var player in players)
+        {
+            positions.Add(player.transform.position);
+        }
+    }
+    private void Update()
+    {
+        if (watchingPlayers)
+        {
+
+        }
     }
 }
