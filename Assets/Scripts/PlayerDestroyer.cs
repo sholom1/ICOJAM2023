c0a58ba4cb4dd9ad24edc1732c33ae263b6bbefd
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2023-07-28 20:17:35 -0400

    Player death 🤯  (#9)
    * Save prefab
    
    * Refactor track
    
    * Add render texture
    
    * Add collider to player
    
    * add particles
    
    * player destroyer in progres
    
    * Add player spawner
    
    * Save track
    
    * Finish player destroyer
    
    * fix issue with player ids
    
    * Add threshold

---
+++ Assets/Scripts/PlayerDestroyer.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PlayerDestroyer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject effect;
+    [SerializeField]
+    private float movementThreshold = 0.1f;
+    private Dictionary<uint, Vector2> positions;
+    private Dictionary<uint, PlayerController_1> playersMarkedForDeath;
+    private bool watchingPlayers;
+    void Start()
+    {
+        Timer.instance.onTimerComplete.AddListener(startListening);
+        Timer.instance.onTimerStart.AddListener(destroyPlayers);
+    }
+    private void startListening()
+    {
+        watchingPlayers = true;
+        positions = new Dictionary<uint, Vector2>();
+        playersMarkedForDeath = new Dictionary<uint, PlayerController_1>();
+        foreach (var player in PlayerManager.instance.players.Values)
+        {
+            positions.Add(player.playerID, player.transform.position);
+        }
+    }
+    private void Update()
+    {
+        if (watchingPlayers)
+        {
+            foreach (var player in PlayerManager.instance.players.Values)
+            {
+                if (positions.TryGetValue(player.playerID, out Vector2 position) && Vector2.Distance(position, (Vector2)player.transform.position) > movementThreshold)
+                {
+                    playersMarkedForDeath.TryAdd(player.playerID, player);
+                    player.deathMark.SetActive(true);
+                }
+            }
+        }
+    }
+    private void destroyPlayers()
+    {
+        watchingPlayers = false;
+        foreach (var player in playersMarkedForDeath.Values)
+        {
+            Destroy(player.gameObject);
+            Destroy(Instantiate(effect, player.transform.position, Quaternion.identity), 1);
+        }
+    }
+}
