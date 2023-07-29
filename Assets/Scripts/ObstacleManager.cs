909651dac84ebb4721030a1111756d8d2d3f0b90
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2023-07-29 19:43:57 -0400

    Finished powerups

--- Assets/Scripts/ObstacleManager.cs
+++ Assets/Scripts/ObstacleManager.cs
@@ -6,6 +6,13 @@ public class ObstacleManager : MonoBehaviour
 {
     [SerializeField]
     private Obstacle[] obstacles;
+    public static ObstacleManager instance;
+    private void Awake()
+    {
+        if (instance)
+            Destroy(instance);
+        instance = this;
+    }
     private void Start()
     {
         Timer.instance.onTimerStart.AddListener(randomlySwap);
--- Assets/Scripts/PowerupSelector.cs
+++ Assets/Scripts/PowerupSelector.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PowerupSelector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Rigidbody2D bulletPrefab;
+    [SerializeField] Collider2D explosionPrefab;
+    enum powerups
     {
-        
+        bullet,
+        explosion,
+        swapObstacles,
+        none
     }
-
-    // Update is called once per frame
-    void Update()
+    private powerups[] avaliablePowerups = { powerups.bullet, powerups.explosion, powerups.swapObstacles };
+    private powerups selectedPowerup;
+    public void OnPowerUp(InputAction.CallbackContext ctx)
     {
-        
+        if (!ctx.ReadValueAsButton()) return;
+        // Todo add cost
+        if (selectedPowerup == powerups.none)
+        {
+            selectedPowerup = avaliablePowerups[Random.Range(0, avaliablePowerups.Length)];
+            return;
+        }
+        switch (selectedPowerup)
+        {
+            case powerups.bullet:
+                var bullet = Instantiate(bulletPrefab, transform.position + transform.up, transform.rotation);
+                bullet.velocity = GetComponent<Rigidbody2D>().velocity * 2;
+                Destroy(bullet.gameObject, 5);
+                break;
+            case powerups.explosion:
+                Physics2D.IgnoreCollision(Instantiate(explosionPrefab, transform.position, Quaternion.identity), GetComponent<Collider2D>());
+                break;
+            case powerups.swapObstacles:
+                ObstacleManager.instance.randomlySwap();
+                break;
+        }
+        selectedPowerup = powerups.none;
     }
 }
