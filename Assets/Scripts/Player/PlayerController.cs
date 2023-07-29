0132a87635a9ea961d66b42d999d235bace5f73d
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2023-07-29 16:32:26 -0400

    Add break mechanic (#10)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,10 +7,13 @@ using UnityEngine.InputSystem;
 
 public class PlayerController_1 : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D rb;
+    [SerializeField] 
+    private Rigidbody2D rb;
+
     public GameObject deathMark;
 
     private Vector2 move_Position;
+    private bool isBreakDepressed;
     public float max_speed;
     public float speed_modifier;
     public float acceleration_mod;
@@ -45,14 +48,13 @@ public class PlayerController_1 : MonoBehaviour
 
     private void MovePlayer()
     {
-
-
-
-        if (Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.y) < max_speed)
+        if (isBreakDepressed)
+            rb.AddForce(-rb.velocity);
+        else if (Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.y) < max_speed)
         {
-            Vector2 speed = move_Position *
-                (speed_modifier + (Mathf.Abs(rb.velocity.x*acceleration_mod) + Mathf.Abs(rb.velocity.y * acceleration_mod)));
-            rb.AddForce(speed , ForceMode2D.Force);
+                Vector2 speed = move_Position *
+               (speed_modifier + (Mathf.Abs(rb.velocity.x * acceleration_mod) + Mathf.Abs(rb.velocity.y * acceleration_mod)));
+                rb.AddForce(speed , ForceMode2D.Force);
         }
     }
 
@@ -71,5 +73,9 @@ public class PlayerController_1 : MonoBehaviour
     {
         //value.x = -value.x;
         move_Position = value;
+    }
+    public void handleBreak(InputAction.CallbackContext ctx)
+    {
+        isBreakDepressed = ctx.ReadValueAsButton();
     }
 }
