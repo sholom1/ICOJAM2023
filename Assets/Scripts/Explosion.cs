9d67ddd0e2b2a2cecf32a0df82f89c3193fcba2b
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2023-07-30 08:04:55 -0400

    Add force instead of push

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -8,6 +8,7 @@ public class Explosion : MonoBehaviour
     [SerializeField] private new CircleCollider2D collider;
     [SerializeField] private float maxRadius;
     [SerializeField] private float speed;
+    [SerializeField] private float explosiveForce;
 
     private void FixedUpdate()
     {
@@ -15,5 +16,10 @@ public class Explosion : MonoBehaviour
         if (maxRadius - collider.radius < .01f)
             Destroy(gameObject);
         RenderCollider.renderCircle(line, transform.position, collider.radius);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Vector2 dir = (collision.transform.position - transform.position).normalized;
+        collision.attachedRigidbody.AddForce(dir * explosiveForce, ForceMode2D.Impulse);
     }
 }
