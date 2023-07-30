c8f25af0607722022fdb30f09638ed5073c18562
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2023-07-30 18:14:18 -0400

    people now get coins

--- Assets/Scripts/Score/RoundManager.cs
+++ Assets/Scripts/Score/RoundManager.cs
@@ -81,6 +81,11 @@ public class RoundManager : MonoBehaviour
 
     public void EndRound()
     {
+        //Award coins
+        foreach (var player in playerManager.players)
+        {
+            scoreKeeper.AddCoins(player.Key, (uint)(player.Value.check_point_num + player.Value.laps_completed * 5));
+        }
         //Searching for the leading player
         PlayerController_1 leadPlayer = null;
         foreach (KeyValuePair<uint, PlayerController_1> player in playerManager.players)
