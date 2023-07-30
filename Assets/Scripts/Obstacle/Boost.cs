675a406250ea9e11e73c6a3e19a2bc8e86864ec0
TheStress <lieu.nelson386@gmail.com>
2023-07-30 19:41:07 -0400

    removing area affecter in code

--- Assets/Scripts/Obstacle/Boost.cs
+++ Assets/Scripts/Obstacle/Boost.cs
@@ -4,20 +4,8 @@ using UnityEngine;
 
 public class Boost : MonoBehaviour
 {
-    AreaEffector2D areaEffector;
-    CircleCollider2D circleCollider;
     [SerializeField]
     float forceAmount;
-    // Start is called before the first frame update
-    void Start()
-    {
-        areaEffector = GetComponent<AreaEffector2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
