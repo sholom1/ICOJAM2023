341219cdb1a8cb80d91c8cbe15dff159e5c51ab4
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2023-07-30 20:11:21 -0400

    Add to joystick

--- Assets/Scripts/Joystick.cs
+++ Assets/Scripts/Joystick.cs
@@ -8,15 +8,21 @@ public class Joystick : MonoBehaviour
     public int stick_id;
     public Vector3 start_vector;
     public PlayerController_1 playerController;
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Material[] powerUpImages;
+    [SerializeField]
+    private MeshRenderer powerUpDisplay;
     // Start is called before the first frame update
     void Start()
     {
-        start_vector = transform.eulerAngles;
+        start_vector = target.eulerAngles;
     }
 
     public void onChangeInput(Vector2 value)
     {
         Vector3 rotation = new Vector3(value.y, 0.0f, -value.x);
-        transform.eulerAngles = start_vector + (rotation * 25);
+        target.eulerAngles = start_vector + (rotation * 25);
     }
 }
